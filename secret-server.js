5a33077d4774b17657890bbe3971b6041b7bc8f0
thomasneplaz <t.neplaz@gmail.com>
2018-11-15 11:53:11 +0100

    exo3 80%

---
+++ secret-server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+var fs = require('fs-extra')
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq'
+
+const port = 4001
+
+const app = express()
+
+let mdp
+
+app.get('/', (req, res) => {
+  mdp = fs.readFileSync(__dirname + '/data/secret.txt','utf8')
+  mdp = encrypt(mdp)
+  fs.writeFileSync(__dirname + '/data/secret.txt',mdp)
+  const decryptsecret = decrypt(fs.readFileSync(__dirname + '/data/secret.txt','utf8'))
+  res.send(`<form action="/" method="GET">
+              <input id="val" type="text" value="${decryptsecret}">
+              <input type="submit" value="Modifier" OnClick="fs.writeFileSync(__dirname + '/data/secret.txt',this)">
+            </form>
+            <br/>
+            <h3>${mdp}</h3>`)
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
+
+
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex')
+  return crypted
+}
+ 
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8')
+  return dec
+}
